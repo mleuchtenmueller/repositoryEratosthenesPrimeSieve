7b837f07f9124dab8b443f0bda0c81bf43d56f4a
mleuchtenmueller <LeuchtenmuellerM1981@sus.htl-grieskirchen.at>
2021-09-27 14:44:29 +0200

    Secound commit

--- EratosthenesPrimeSieve/src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve/src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
@@ -18,14 +18,25 @@ public class EratosthenesPrimeSieve implements PrimeSieve {
     private boolean[] primeList = new boolean[oG];
 
     public void fillPrimeList() {
-        for (int i = 0; i < primeList.length; i++) {
+        for (int i = 2; i < primeList.length; i++) {
             primeList[i] = true;
         }
     }
 
     @Override
     public boolean isPrime(int p) {
-        return true;
+        for (int i = 2; i < primeList.length; i++) {
+            if (primeList[i]) {
+                int x = i * i;
+                for (int j = 0; j < oG; j++) {
+                    if (x == j) {
+                        primeList[j] = false;
+                        x = x + i;
+                    }
+                }
+            }
+        }
+        return primeList[p];
     }
 
     @Override
