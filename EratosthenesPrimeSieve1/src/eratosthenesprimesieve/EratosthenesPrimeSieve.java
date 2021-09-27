6e6b588bebc6b54d5ea6e3afc1de18c99e5051c9
mleuchtenmueller <LeuchtenmuellerM1981@sus.htl-grieskirchen.at>
2021-09-27 22:15:55 +0200

    Fourth commit

--- EratosthenesPrimeSieve1/src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve1/src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
@@ -1,5 +1,5 @@
 package eratosthenesprimesieve;
-
+import java.util.*;
 /**
  *
  * @author Moritz
@@ -9,44 +9,79 @@ public class EratosthenesPrimeSieve implements PrimeSieve {
     /**
      * @param args the command line arguments
      */
-    private int oG;
+    private int uL;
 
-    public EratosthenesPrimeSieve(int oG) {
-        this.oG = oG;
+    public EratosthenesPrimeSieve(int uL) {
+        this.uL = uL;
     }
 
-    private boolean[] primeList = new boolean[oG];
+    public void main()
+    {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("1...check if a number is a prime number");
+      System.out.println("2...Show all prime numbers up to the upper limit");
+      System.out.println("3...Find 2 prime numbers for each even natural number");
+      int input = Integer.parseInt(scanner.nextLine());
+      if(input == 1)
+      {
+        isPrime(int n);
+      }
+      else if(input == 2)
+      {
+        printPrimes();
+      }
+      else if(input == 3)
+      {
+         aufgabe2();
+      }
+    }
+
+    private boolean[] boolPrimeList = new boolean[uL];
 
     public void fillPrimeList() {
-        for (int i = 2; i < primeList.length; i++) {
-            primeList[i] = true;
+        for (int i = 2; i < boolPrimeList.length; i++) {
+            boolPrimeList[i] = true;
         }
     }
 
     @Override
     public boolean isPrime(int p) {
-        for (int i = 2; i < primeList.length; i++) {
-            if (primeList[i]) {
+        for (int i = 2; i < boolPrimeList.length; i++) {
+            if (boolPrimeList[i]) {
                 int x = i * i;
                 for (int j = 0; j < oG; j++) {
                     if (x == j) {
-                        primeList[j] = false;
+                        boolPrimeList[j] = false;
                         x = x + i;
                     }
                 }
             }
         }
-        return primeList[p];
+        return boolPrimeList[p];
     }
 
     @Override
     public void printPrimes() {
-        for(int i = 0; i < primeList.length; i++)
+        for(int i = 0; i < boolPrimeList.length; i++)
         {
-            if(primeList[i])
+            if(boolPrimeList[i])
             {
               System.out.println(i);
             }
         }
+    }
+
+    //Augabe 2
+    public void aufgabe2(){
+    List<int> primes = new ArrayList<int>();
+
+        for(int i = 0; i < boolPrimeList.length; i++)
+        {
+            if(boolPrimeList[i])
+            {
+                primes.add(i);
+            }
+        }
+
     }
 }
